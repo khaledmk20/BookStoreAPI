494c178037d3e1652cd4b8d11a21bf493be65eff
Khaled Abdelalim <khaled.korany2004@gmail.com>
2024-05-30 20:17:07 +0300

    added JWT configuration for swagger

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,6 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStoreAPI.Models
 {
     public class User
@@ -6,7 +8,9 @@ namespace BookStoreAPI.Models
         public int Id { get; set; }
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
+        [EmailAddress]
         public string Email { get; set; } = "";
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone no.")]
         public string PhoneNumber { get; set; } = "";
         public string Gender { get; set; } = "";
         public string UserRole { get; set; } = "";
--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using Stripe;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 string? tokenKeyString = builder.Configuration.GetSection("Jwt:Key").Value;
 
@@ -45,6 +72,7 @@ if (app.Environment.IsProduction())
 {
     app.UseHttpsRedirection();
 }
+
 
 
 app.UseAuthentication();
